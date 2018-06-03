ce97eae0700df07bb83591e7e840393141a8fdbc
qpwo5470 <37436911+qpwo5470@users.noreply.github.com>
2018-06-04 08:55:09 +0900

    Initial commit

---
+++ saveImage.php
@@ -0,0 +1,9 @@
+﻿<?php
+$img = $_POST['data'];
+$img = str_replace('data:image/png;base64,', '', $img);
+$img = str_replace(' ', '+', $img);
+$fileData = base64_decode($img);
+//saving
+$fileName = 'cards/'.'Card'.date("Y_m_d").'.png';
+file_put_contents($fileName, $fileData);
+?>
